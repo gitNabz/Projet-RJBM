2a24299c27fd44bad704621083e7fc0bb0112835
gitNabz <nabzgc@gmail.com>
2018-03-09 13:35:20 +0100

    update

--- pong.js
+++ pong.js
@@ -4,7 +4,7 @@ var c=document.getElementById("game");
 var ctx=c.getContext("2d");
 ctx.rect(20,20,700,500);
 ctx.fillStyle="#111111";
-ctx.strokeStyle="red";
+ctx.strokeStyle="green";
 ctx.fill();
 ctx.lineWidth="5";
 ctx.lineCap = "round";
